8178575f7ca72650aea7aed13df8bc6f804cbf5a
Lizza Novikova <134241619+lnxvi@users.noreply.github.com>
2025-04-14 17:27:42 -0400

    update opt test

--- DesignChallenge2/opt_test.c
+++ DesignChallenge2/opt_test.c
@@ -19,21 +19,31 @@
 #define CH  3
 
 
- uint32_t distance[CH];
+uint32_t distance[CH];
+uint32_t amplitude[CH];
 
 int main(void) {
 
     Clock_Init48MHz();
+    printf("hello\n");
     SysTick_Init();
     I2CB1_Init(30);
+    OPT3101_Init();
+    OPT3101_Setup();
+    OPT3101_CalibrateInternalCrosstalk();
 
+    //printf("starting loop\n");
     while(1) {
         uint32_t i;
         for (i = 0; i < CH; i++) {
+            //printf("starting measurement\n");
             OPT3101_StartMeasurementChannel(i);
+            //printf("waiting for busy-wait\n");
             while(!OPT3101_CheckDistanceSensor()) {}
+            //printf("busy wait complete\n");
+            OPT3101_GetMeasurement(distance, amplitude);
             distance[i] = OPT3101_GetDistanceMillimeters();
-            printf("%d\n", distance[i]);
+            printf("channel %d: %d\n", i, distance[i]);
         }
         SysTick_Wait10ms(100);
     }
