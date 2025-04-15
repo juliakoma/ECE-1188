af8e824fb11402f1f81e88ef7850df5e88492268
juliakoma <124528389+juliakoma@users.noreply.github.com>
2025-04-15 17:14:10 -0400

    Update main.c

--- DesignChallenge2/main.c
+++ DesignChallenge2/main.c
@@ -28,36 +28,46 @@ void Bump_Handler(uint8_t bumpData) {
     Motor_Stop();
 }
 
+
 void main(void) {
 
     Clock_Init48MHz();
     UART0_Init();    // UART for Bluetooth
     Motor_Init();    // PWM motor setup
-    //Sensor_Init(); // Bump and Reflectance
-    //Motor_Forward(2000, 2000);
-    char cmd;
+    Sensor_Init();   // Bump and Reflectance
+    //EnableInterrupts();
 
     while (1) {
         BlueTooth_Handler();
 
+        // Bump Occurred
         if (collisionFlag) {
             collisionFlag = 0;
             goFlag = 0;
             continue;
         }
 
-        if (goFlag == 1){
-            // Moving Logic here
-            Motor_Forward(1700, 1700);
+        // Bluetooth command is go
+        if (goFlag == 1) {
+            uint8_t reflectance = Reflectance_Read(2000);
 
+            // Reached finish line
+            if (reflectance > 0xE7) {
+                UART0_OutString("Black line detected! Stopping...\n\r");
+                Motor_Stop();
+                goFlag = 0;
+            } else {
+                Motor_Forward(DUTY_LEFT, DUTY_RIGHT);
+            }
         }
 
-        if (goFlag == 0){
-            // Stop Logic here
+        // Bluetooth command is stop
+        if (goFlag == 0) {
             Motor_Stop();
         }
     }
 }
+
 
 // Initialize Sensors
 void Sensor_Init(void) {
@@ -75,7 +85,7 @@ void HandleCollision(uint8_t bumpSensor) {
 void BlueTooth_Handler(){
     char cmd;
     if (UART0_CharAvailable()) {
-        char cmd = UART0_InChar();
+        cmd = UART0_InChar();
         UART0_OutString("Command Received is: ");
         UART0_OutChar(cmd);
 
